0cb8b7a24d3548554412d078665c193055e1b7af
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-07-25 15:42:11 +0900

    feat: 설문조사, 사전테스트 엔티티 초기 코드 작성

--- Back/src/main/java/com/kkosunnae/deryeogage/domain/survey/SurveyEntity.java
+++ Back/src/main/java/com/kkosunnae/deryeogage/domain/survey/SurveyEntity.java
@@ -1,4 +1,33 @@
 package com.kkosunnae.deryeogage.domain.survey;
 
+import com.kkosunnae.deryeogage.domain.user.UserEntity;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
 public class SurveyEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "friendly")
+    private Character friendly;
+
+    @Column(name = "activity")
+    private Character activity;
+
+    @Column(name = "dependency")
+    private Character dependency;
+
+    @Column(name = "hair")
+    private Character hair;
+
+    @Column(name = "bark")
+    private Character bark;
 }
