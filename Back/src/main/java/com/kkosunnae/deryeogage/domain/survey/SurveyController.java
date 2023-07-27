78a96e1d4fe00f9885e9cc58f77b93c033bd620e
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-07-27 21:12:05 +0900

    refactor: 설문조사 API 에러 수정 완료
    3개 api 전부 postman 테스트 완료함.

--- Back/src/main/java/com/kkosunnae/deryeogage/domain/survey/SurveyController.java
+++ Back/src/main/java/com/kkosunnae/deryeogage/domain/survey/SurveyController.java
@@ -2,10 +2,11 @@ package com.kkosunnae.deryeogage.domain.survey;
 
 import com.kkosunnae.deryeogage.global.util.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RequiredArgsConstructor
 @RestController
 public class SurveyController {
@@ -13,19 +14,20 @@ public class SurveyController {
     private final SurveyService surveyService;
 
     @GetMapping("/surveys/{userId}")
-    public Response<SurveyDto> getSurvey(@RequestParam Long userId) {
+    public Response<SurveyDto> getSurvey(@PathVariable Long userId) {
         SurveyDto survey = surveyService.getSurvey(userId);
         return Response.success(survey);
     }
 
     @PostMapping("/surveys")
-    public Response<Object> saveSurvey(SurveyDto surveyDto) {
+    public Response<Object> saveSurvey(@RequestBody SurveyDto surveyDto) {
+        log.info("설문조사 dto 정보 : " + surveyDto.getActivity());
         surveyService.save(surveyDto);
         return Response.success(null);
     }
 
     @PutMapping("/surveys/{userId}")
-    public Response<Object> update(@RequestParam Long userId, SurveyDto surveyDto) {
+    public Response<Object> update(@PathVariable Long userId, @RequestBody SurveyDto surveyDto) {
         surveyService.update(userId, surveyDto);
         return Response.success(null);
     }
