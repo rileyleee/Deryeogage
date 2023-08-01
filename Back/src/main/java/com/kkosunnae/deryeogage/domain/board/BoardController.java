762ea1491ac22261ac2bbbe553a34f7e3be4f578
이은경 <eunclid0202@gmail.com>
2023-08-01 13:14:58 +0900

    feat: (파일업로드) 게시판 등록 시 파일 업로드 -> 파일 S3연동 및 DB 저장 테스트 완료       - 입양 미션 파일업로드 위해 S3 관련 임시 api 저장       - WebConfig 인터셉터 제외 uri 및 cors mapping method 'OPTIONS'추가       - DB 파일명 데이터 타입 변경 필요       - 프론트에서 백 서버 요청 시 헤더 특이사항         - ContentType : multipart/form-data

--- Back/src/main/java/com/kkosunnae/deryeogage/domain/board/BoardController.java
+++ Back/src/main/java/com/kkosunnae/deryeogage/domain/board/BoardController.java
@@ -12,8 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
