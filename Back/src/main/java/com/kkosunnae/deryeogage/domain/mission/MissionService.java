37e0746a2f350f57813c8408290b48137dd2e2ca
이은경 <eunclid0202@gmail.com>
2023-08-05 12:10:50 +0900

    feat: (입양 미션) 어노테이션 및 주석 추가

--- Back/src/main/java/com/kkosunnae/deryeogage/domain/mission/MissionService.java
+++ Back/src/main/java/com/kkosunnae/deryeogage/domain/mission/MissionService.java
@@ -2,7 +2,6 @@ package com.kkosunnae.deryeogage.domain.mission;
 
 import com.kkosunnae.deryeogage.domain.adopt.AdoptRepository;
 import com.kkosunnae.deryeogage.domain.user.UserRepository;
-import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,7 +17,7 @@ public class MissionService {
     private final AdoptRepository adoptRepository;
     private final MissionRepository missionRepository;
 
-    //입양확정 시 미션 최초 생성
+    //입양확정 시 미션 최초 생성(adoptService의 updateAdopt에서 호출)
     public MissionEntity save(Long missionUserId, Integer adoptId) {
 
         MissionDto missionDto = new MissionDto(missionUserId);
