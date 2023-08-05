a48f26e24454169deb36cb94d236afca87a039e3
이은경 <eunclid0202@gmail.com>
2023-08-06 00:12:26 +0900

    feat: (미션) 조회, 등록, 삭제 기능 구현

--- Back/src/main/java/com/kkosunnae/deryeogage/domain/mission/MissionService.java
+++ Back/src/main/java/com/kkosunnae/deryeogage/domain/mission/MissionService.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Map;
+
 @Slf4j
 @Service
 @Transactional
@@ -25,5 +28,88 @@ public class MissionService {
         MissionEntity mission = missionRepository.save(missionDto.toEntity(userRepository, adoptRepository));
 
         return mission;
+    }
+
+    // 미션 수행 여부 확인(후책임비 반환 시 호출)
+    public boolean missionCheck(Integer missionId) {
+
+        MissionEntity missionEntity = missionRepository.findById(missionId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 미션이 존재하지 않습니다. missionId: " + missionId));
+
+        return missionEntity.getMissionUrl1() != null &&
+                missionEntity.getMissionUrl2() != null &&
+                missionEntity.getMissionUrl3() != null &&
+                missionEntity.getMissionUrl4() != null;
+    }
+
+    // 한 개의 입양 내역에 대한 미션 조회
+    public MissionDto getMission(Integer missionId) {
+
+        MissionEntity missionEntity = missionRepository.findById(missionId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 미션이 존재하지 않습니다. missionId: " + missionId));
+
+        return missionEntity.toDto();
+    }
+
+    // 한 개의 입양 내역 중 각각의 미션 단계에 대한 파일 등록
+    public String registOne(MissionDto missionDto, int urlId, Map<String, List> nameList) {
+
+        List<String> savedPaths = nameList.get("path");
+        switch (urlId) {
+            case 1:
+                missionDto.setMissionUrl1(savedPaths.get(0));
+                break;
+            case 2:
+                missionDto.setMissionUrl2(savedPaths.get(0));
+                break;
+            case 3:
+                missionDto.setMissionUrl3(savedPaths.get(0));
+                break;
+            case 4:
+                missionDto.setMissionUrl4(savedPaths.get(0));
+                break;
+            default:
+                throw new IllegalArgumentException("존재하지 않는 미션 단계입니다.: " + urlId);
+        }
+
+        Integer missionId = missionDto.getId();
+
+        MissionEntity eachMission = missionRepository.findById(missionId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 미션이 존재하지 않습니다. missionId: " + missionId));
+
+        missionRepository.save(eachMission);
+
+        return savedPaths.get(0);
+    }
+
+    // 한 개의 입양 내역 중 각각의 미션 단계에 대한 파일 삭제
+
+    public void deleteOne(MissionDto missionDto, int urlId) {
+
+        // DB 삭제
+        switch (urlId) {
+            case 1:
+                missionDto.setMissionUrl1(null);
+                break;
+            case 2:
+                missionDto.setMissionUrl2(null);
+                break;
+            case 3:
+                missionDto.setMissionUrl3(null);
+                break;
+            case 4:
+                missionDto.setMissionUrl4(null);
+                break;
+            default:
+                throw new IllegalArgumentException("존재하지 않는 미션 단계입니다.: " + urlId);
+        }
+
+        Integer missionId = missionDto.getId();
+        MissionEntity eachMission = missionRepository.findById(missionId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 미션이 존재하지 않습니다. missionId: " + missionId));
+
+        missionRepository.save(eachMission);
+
+        //3S 삭제는 아직
     }
 }
