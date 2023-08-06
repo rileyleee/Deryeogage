73641d7e9686ecde8ed9fc315b100ce5c1d602a9
이은경 <eunclid0202@gmail.com>
2023-08-06 17:07:55 +0900

    chore: (인터셉터) 게시글 상세조회 인터셉터 배제 처리

--- Back/src/main/java/com/kkosunnae/deryeogage/global/config/WebConfig.java
+++ Back/src/main/java/com/kkosunnae/deryeogage/global/config/WebConfig.java
@@ -34,8 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "/webjars/**",
                         "/api/users/oauth",
                         "/api/boards/list",
-                        "/api/boards/**", //배제 인터셉터 세세하게 작성 필요
-                        "/api/files/**");
+                        "/api/boards/each/**"); //추천 알고리즘은 추가해야 하는가
             
     }
 
