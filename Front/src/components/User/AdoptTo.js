f2064c6d24c4b303441312ccfe3bad353b805222
ksykma <qkdxks0922@gmail.com>
2023-08-11 10:00:07 +0900

    design: 마이페이지 css 수정

--- Front/src/components/User/AdoptTo.js
+++ Front/src/components/User/AdoptTo.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import styled from "styled-components";
+import * as S from "../../styled/User/AdoptTo.style"
 import MissionList from "./MissionList";
-import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function AdoptTo() {
   const [adopts, setAdopts] = useState([]);
@@ -10,6 +10,7 @@ function AdoptTo() {
   const [selectedMissionId, setSelectedMissionId] = useState(null);
   const [selectedIndex, setSelectedIndex] = useState(null);
   console.log("=================adopts: ", adopts);
+  const nickname = localStorage.getItem('nickname')
 
   const handleMissionClick = (missionId, index) => {
     setShowMissionModal(true);
@@ -86,43 +87,43 @@ function AdoptTo() {
       );
       console.log("====================adoptTo:", response.data.data);
 
-      const adoptsWithBoardInfo = await Promise.all(
-        response.data.data.map(async (adopt) => {
-          const matchingBoard = boardResponse.data.data.find(
-            (board) => board.id === adopt.boardId
+    const adoptsWithBoardInfo = await Promise.all(
+      response.data.data.map(async (adopt) => {
+        const matchingBoard = boardResponse.data.data.find(
+          (board) => board.id === adopt.boardId
+        );
+
+        let completedMissions = 0;
+        if (adopt.missionId !== null) {
+          // missionId가 null이 아닌 경우에만 요청
+          // 미션 정보를 가져옴
+          const missionResponse = await axios.get(
+            `${REACT_APP_API_URL}/missions/${adopt.missionId}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
           );
 
-          let completedMissions = 0;
-          if (adopt.missionId !== null) {
-            // missionId가 null이 아닌 경우에만 요청
-            // 미션 정보를 가져옴
-            const missionResponse = await axios.get(
-              `${REACT_APP_API_URL}/missions/${adopt.missionId}`,
-              {
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                },
-              }
-            );
+          // 완료된 미션 수를 계산
+          completedMissions = [
+            missionResponse.data.data.missionUrl1,
+            missionResponse.data.data.missionUrl2,
+            missionResponse.data.data.missionUrl3,
+            missionResponse.data.data.missionUrl4,
+          ].filter((url) => url !== null).length; // null이 아닌 갯수를 세서 완료된 미션의 수를 계산
+        }
 
-            // 완료된 미션 수를 계산
-            completedMissions = [
-              missionResponse.data.data.missionUrl1,
-              missionResponse.data.data.missionUrl2,
-              missionResponse.data.data.missionUrl3,
-              missionResponse.data.data.missionUrl4,
-            ].filter((url) => url !== null).length; // null이 아닌 갯수를 세서 완료된 미션의 수를 계산
-          }
-
-          return {
-            ...adopt,
-            boardInfo: matchingBoard,
-            imageUrl: matchingBoard?.fileList[0],
-            completedMissions, // 완료된 미션 수
-            toConfirmYn: adopt.toConfirmYn, // 입양 확정 여부를 추가
-          };
-        })
-      );
+        return {
+          ...adopt,
+          boardInfo: matchingBoard,
+          imageUrl: matchingBoard?.fileList[0],
+          completedMissions, // 완료된 미션 수
+          toConfirmYn: adopt.toConfirmYn, // 입양 확정 여부를 추가
+        };
+      })
+    );
 
       setAdopts(adoptsWithBoardInfo);
     } catch (error) {
@@ -137,172 +138,78 @@ function AdoptTo() {
   const Media = ({ src }) => {
     if (src.endsWith(".mp4")) {
       return (
-        <MediaContainer>
-          <StyledVideo src={src} controls muted />
-        </MediaContainer>
+        <div className="col-1">
+          <S.StyledVideo src={src} controls muted />
+        </div>
       );
     }
     return (
-      <MediaContainer>
-        <StyledImage src={src} alt="board" />
-      </MediaContainer>
+      <div className="col-1">
+        <S.StyledImage src={src} alt="board" />
+      </div>
     );
   };
 
   return (
-    <div>
-      <h2>입양 내역</h2>
+    <div className="container">
+      <S.BoardRow className="row list">
+        <div className="col-2 text-center">대표 이미지</div>
+        <div className="col-4 text-center">입양글 제목</div>
+        <div className="col-3 text-center">입양 확정 내역</div>
+        {/* <div className="col-2 text-center">입양자</div> */}
+        <div className="col-3 text-center">입양 미션</div>
+      </S.BoardRow>
+      <S.ScrollBar>
       {adopts.length === 0 ? (
-        <p>입양 내역이 없습니다.</p>
+        <div className="text-center">입양 내역이 없습니다.</div>
       ) : (
         adopts.map((adopt, index) => (
-          <AdoptToCard key={index}>
-            <Media src={adopt.imageUrl} />
-            <Link to={`/adopt/${adopt.boardId}`}>
-              <Title>{adopt.boardInfo?.title}</Title>
-            </Link>
+          <S.BoardRow className="row item" key={index}>
+            <Media className="col-2" src={adopt.imageUrl} />
+            <S.TitleLink className="col-4 text-center" to={`/adopt/${adopt.boardId}`}>{adopt.boardInfo?.title}</S.TitleLink>
             {adopt.toConfirmYn ? ( // toConfirmYn 값에 따라 버튼을 표시
-              <ConfirmedButton>입양 확정 완료</ConfirmedButton>
+              <S.ConfirmedButton className="col-3 text-center">입양 확정 완료</S.ConfirmedButton>
             ) : (
-              <ConfirmButton
+              <S.ConfirmButton className="col-3 text-center"
                 onClick={() => handleConfirmAdoption(adopt.id, index)}
               >
                 입양 확정하기
-              </ConfirmButton>
+              </S.ConfirmButton>
             )}
-            {adopt.status === "arrive" &&
+            {/* <div className="col-2 text-center">{adopts[0].boardInfo.userNickname}</div>
+            <div className="col-2 text-center">{nickname}</div> */}
+            {adopt.status === "arrive" ?
               (adopt.completedMissions === 4 ? (
-                <ResponsibilityButton
+                <S.ResponsibilityButton className="col-3 text-center"
                   onClick={() => handleResponsibilityFeeReturn(adopt.boardId)}
                 >
                   책임비 반환하기
-                </ResponsibilityButton>
+                </S.ResponsibilityButton>
               ) : (
-                <MissionButton
+                <S.MissionButton className="col-3 text-center"
                   onClick={() => handleMissionClick(adopt.missionId, index)}
                 >
                   입양 미션하기 ({adopt.completedMissions}/4)
-                </MissionButton>
-              ))}
-          </AdoptToCard>
+                </S.MissionButton>
+              )) : <div className="col-3 text-center">입양 예정</div>}
+          </S.BoardRow>
         ))
       )}
+      </S.ScrollBar>
       {showMissionModal && (
-        <MissionModal>
-          <MissionContent>
+        <S.MissionModal>
+          <S.MissionContent>
             <MissionList
               missionId={selectedMissionId}
               completedMissions={adopts[selectedIndex]?.completedMissions}
               fetchAdopts={fetchAdopts} // 이 줄을 추가하세요
             />
-            <CloseButton onClick={closeModal}>닫기</CloseButton>
-          </MissionContent>
-        </MissionModal>
+            <S.CloseButton onClick={closeModal}>닫기</S.CloseButton>
+          </S.MissionContent>
+        </S.MissionModal>
       )}
     </div>
   );
 }
 
 export default AdoptTo;
-
-// 스타일 컴포넌트 정의
-const MissionButton = styled.button`
-  background-color: #4caf50;
-  color: white;
-  padding: 10px 20px;
-  margin: 10px;
-  border: none;
-  cursor: pointer;
-  &:hover {
-    background-color: #45a049;
-  }
-`;
-
-const MissionModal = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const CloseButton = styled.button`
-  background-color: #f44336;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  cursor: pointer;
-  &:hover {
-    background-color: #da190b;
-  }
-`;
-
-const MissionContent = styled.div`
-  background-color: white;
-  padding: 20px;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ConfirmButton = styled.button`
-  background-color: #ff5722;
-  color: white;
-  padding: 10px 20px;
-  margin: 10px;
-  border: none;
-  cursor: pointer;
-  &:hover {
-    background-color: #e64a19;
-  }
-`;
-
-const ConfirmedButton = styled.button`
-  background-color: #ccc;
-  color: white;
-  padding: 10px 20px;
-  margin: 10px;
-  border: none;
-`;
-
-const AdoptToCard = styled.div`
-  display: flex;
-  align-items: center; // 세로 정렬
-  margin: 10px 0;
-`;
-
-const MediaContainer = styled.div`
-  width: 100px;
-  height: 100px;
-  margin-right: 20px; // 우측 여백
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const StyledImage = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover; // 이미지 비율 유지
-`;
-
-const StyledVideo = styled.video`
-  width: 100%;
-  height: 100%;
-  object-fit: cover; // 비디오 비율 유지
-`;
-
-const Title = styled.h3`
-  text-decoration: none; // 밑줄 표시
-  cursor: pointer; // 포인터 마우스 커서
-`;
-
-const ResponsibilityButton = styled.button`
-  // 여기에 필요한 스타일을 적용하세요.
-`;
