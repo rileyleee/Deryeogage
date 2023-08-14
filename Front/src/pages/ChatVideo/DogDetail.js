9a4614c5a1f3fd5bebc30a18205e79cb87a1c46e
이수연 <dltd0912@gmail.com>
2023-08-14 23:59:23 +0900

    feat: 동교 CSS 적용

--- Front/src/pages/ChatVideo/DogDetail.js
+++ Front/src/pages/ChatVideo/DogDetail.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate, Link, useLocation } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import * as S from "../../styled/ChatVideo/DogDetail.style"
 import ResultPaw from "./../../components/ResultPaw";
@@ -7,22 +7,15 @@ import { Carousel } from "react-bootstrap";
 
 function DogDetail(props) {
   const [adoptData, setAdoptData] = useState(null);
+  const { boardId } = props;
   const token = localStorage.getItem("accessToken");
   const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  
-  // URL에서 boardId를 먼저 찾음
-  let boardIdFromQuery = queryParams.get('boardId');
-  
-  // URL에서 boardId를 찾지 못했으면 props에서 가져옴
-  const effectiveBoardId = boardIdFromQuery || props.boardId;
 
   useEffect(() => {
     const fetchAdoptData = async () => {
       try {
         const response = await axios.get(
-          `${REACT_APP_API_URL}/boards/each/${effectiveBoardId}`,
+          `${REACT_APP_API_URL}/boards/each/${boardId}`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -39,23 +32,14 @@ function DogDetail(props) {
     };
 
     fetchAdoptData();
-  }, []);
+  }, [boardId]);
 
   return (
     <div>
       {adoptData ? (
         <S.Container>
-          강쥐 상세정보 보여줄거임
-          {adoptData.board.title}
+          <h4>{adoptData.board.title}</h4>
           <S.FlexContainer>
-            <S.Box>
-              {/* 강아지 특성 정보를 표시하는 섹션 */}
-              <ResultPaw title="친화력" selected={adoptData.board.friendly} small={true} />
-              <ResultPaw title="활동량" selected={adoptData.board.activity} small={true} />
-              <ResultPaw title="의존도" selected={adoptData.board.dependency} small={true} />
-              <ResultPaw title="왈왈왈" selected={adoptData.board.bark} small={true} />
-              <ResultPaw title="털빠짐" selected={adoptData.board.hair} small={true} />
-            </S.Box>
             <S.Box>
               <Carousel>
                 {Object.entries(adoptData.images).map(
@@ -78,21 +62,30 @@ function DogDetail(props) {
                   }
                 )}
               </Carousel>
-              {/* 강아지 기본 정보를 표시하는 섹션 */}
-              <p>이름 :{adoptData.board.name}</p>
-              <p>나이 :{adoptData.board.age}세</p>
-              <p>지역 :{adoptData.board.regionCode}</p>
-              <p>성별 :{adoptData.board.gender ? "남자" : "여자"}</p>
-              <p>
-                칩 등록 여부 :
-                {adoptData.board.chipYn ? "등록" : "미등록(알 수 없음)"}
-              </p>
+              <S.DogInfo>
+                {/* 강아지 기본 정보를 표시하는 섹션 */}
+                <p>이 름 : {adoptData.board.name}</p>
+                <p>나 이 : {adoptData.board.age} 세</p>
+                <p>지 역 : {adoptData.board.regionCode}</p>
+                <p>성 별 : {adoptData.board.gender ? "남자" : "여자"}</p>
+                <p>
+                  칩 등록 여부 : {adoptData.board.chipYn ? "등록" : "미등록(알 수 없음)"}
+                </p>
+              </S.DogInfo>
+            </S.Box>
+            <S.Box>
+              {/* 강아지 특성 정보를 표시하는 섹션 */}
+              <ResultPaw title="친화력" selected={adoptData.board.friendly} small={true} />
+              <ResultPaw title="활동량" selected={adoptData.board.activity} small={true} />
+              <ResultPaw title="의존도" selected={adoptData.board.dependency} small={true} />
+              <ResultPaw title="왈왈왈" selected={adoptData.board.bark} small={true} />
+              <ResultPaw title="털빠짐" selected={adoptData.board.hair} small={true} />
             </S.Box>
           </S.FlexContainer>
           <S.Span>건강정보</S.Span>
+           <S.infoBox>
           {adoptData.board.health}
-          <S.Span>소개</S.Span>
-          {adoptData.board.introduction}
+          </S.infoBox>
           <S.FixedButton onClick={() => {
             // 화상 채팅을 시작하면 showVideoRoom을 true로 설정
             props.setShowVideoRoom(true);
